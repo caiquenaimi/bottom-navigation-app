18860b0b9256bbe2710af9a7ca47a65295d76b94
caiquenaimi <caiquenaimi.564@gmail.com>
2024-03-06 08:43:04 -0300

    Adiciona ícones de navegação na barra inferior

--- App.jsx
+++ App.jsx
@@ -5,20 +5,49 @@ import Contact from "./src/screens/Contact";
 import Home from "./src/screens/Home";
 import Profile from "./src/screens/Profile";
 import { NavigationContainer } from "@react-navigation/native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 export default function App() {
   const Tab = createBottomTabNavigator();
   return (
-    <NavigationContainer>
+    <NavigationContainer
+    >
       <Tab.Navigator
         initialRouteName="Home"
-        activeColor="#f0edf6"
-        inactiveColor="#3e2465"
-        barStyle={{ backgroundColor: '#694fad' }}
+        tabBarOptions={{
+          activeTintColor: "#e91e63",
+        }}
       >
-        <Tab.Screen name="Home" component={Home}  />
-        <Tab.Screen name="Contact" component={Contact} />
-        <Tab.Screen name="Profile" component={Profile} />
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarLabel: "Home",
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="home" color={color} size={26} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Contact"
+          component={Contact}
+          options={{
+            tabBarLabel: "Contact",
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="bell" color={color} size={26} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={Profile}
+          options={{
+            tabBarLabel: "Profile",
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="account" color={color} size={26} />
+            ),
+          }}
+        />
       </Tab.Navigator>
       <StatusBar style="auto" />
     </NavigationContainer>
